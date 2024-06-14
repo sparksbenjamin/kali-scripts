f5b930652899948165495ad0d26940c36ae168ef
Ben Sparks <sparksbenjamin@gmail.com>
2024-06-14 10:09:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 Echo "Install SecLists (https://github.com/danielmiessler/SecLists)"
-sudo apt install SecLists
-echo "Installing Aliases"
-wget "https://raw.githubusercontent.com/sparksbenjamin/kali-scripts/master/aliases.zsh" ~/kali_aliases.zsh
-tee -a "source ~/kali_aliases.zsh" > ~/.zshrc
-
+sudo apt install seclists 
+REPO_URL="https://github.com/sparksbenjamin/kali-scripts.git"
+DIR_NAME="kali-scripts"
+if [ -d "$DIR_NAME" ]; then
+    echo "Directory $DIR_NAME exists. Updating the repository."
+    cd $DIR_NAME
+    git pull
+else
+    echo "Directory $DIR_NAME does not exist. Cloning the repository."
+    git clone $REPO_URL $DIR_NAME
+fi

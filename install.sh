ed0f825e551a7ccd5e316321c260cdcb17c8c4cf
Ben Sparks <sparksbenjamin@gmail.com>
2024-06-14 09:46:02 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+sudo apt get install SecLists
 echo "Installing Aliases"
 wget "https://raw.githubusercontent.com/sparksbenjamin/kali-scripts/master/aliases.zsh" ~/kali_aliases.zsh
 tee -a "source ~/kali_aliases.zsh" > ~/.zshrc

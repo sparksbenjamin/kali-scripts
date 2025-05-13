6aa4a434d7c9186e38701ead95f8691946f5b551
Ben Sparks <sparksbenjamin@gmail.com>
2025-05-13 13:51:42 -0400

    Added VSCode
    Added Code from MS Repo.

--- install.sh
+++ install.sh
@@ -26,7 +26,11 @@ source vpn_env311/bin/activate
 pip install "openconnect-sso[full]"
 echo "Installing Brave"
 curl -fsS https://dl.brave.com/install.sh | sh
-
+echo "Installing VSCode"
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
 # Check if the directory exists
 if [ -d "$DIR_NAME" ]; then
     echo "Directory $DIR_NAME exists. Updating the repository."
